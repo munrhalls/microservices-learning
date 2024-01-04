74ce7cc9de2215127994e805b56cc801164ff293
Munrhalls <antarcticdepths71@gmail.com>
2024-01-05 00:18:55 +0100

    start

--- client/server.js
+++ client/server.js
@@ -1,1 +1,19 @@
-console.log("server");
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+http
+  .createServer((req, res) => {
+    const filePath = path.join(__dirname, "index.html");
+    const contentType = "text/html";
+
+    fs.readFile(filePath, (err, content) => {
+      if (err) {
+        res.writeHead(500);
+        res.end("Error loading index.html");
+      } else {
+        res.writeHead(200, { "Content-Type": contentType });
+        res.end(content, "utf-8");
+      }
+    });
+  })
+  .listen(3000, () => console.log("Server is running on port 3000"));
---
+++ readme.md
@@ -0,0 +1,5 @@
+Objectives:
+
+- learning making a service
+- learning frontend service displaying data from another service
+- learning combining two backend services into a nested structure
