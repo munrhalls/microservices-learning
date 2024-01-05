298236459dd455d422fb6e8f61a4880ff273465e
Munrhalls <antarcticdepths71@gmail.com>
2024-01-05 03:52:35 +0100

    Service A (client) requests service B (posts) to update its data! And then, service A (client) also requests service B to give it posts list!

--- client/index.js
+++ client/index.js
@@ -1,19 +1,43 @@
+const populatePosts = function (posts) {
+  const postsContainer = document.getElementById("postsContainer");
+  let postsDisplay = ``;
+
+  posts.forEach((post) => {
+    postsDisplay += `
+    <div class="col-4">
+        <div class="card  mt-2 mb-2 p-2">
+            <h3>${post.title}</h3>
+        </div>
+    </div>
+        `;
+  });
+  postsContainer.innerHTML = postsDisplay;
+};
+
+const getPosts = async function () {
+  const response = await fetch("http://localhost:4000/posts");
+  const data = await response.json();
+  return data;
+};
+
 const addPost = async function (e) {
   e.preventDefault();
   const form = document.getElementById("formNewPost");
   const formData = new FormData(form);
-  const post = formData.get("post");
+  const title = formData.get("title");
 
   const response = await fetch("http://localhost:4000/posts", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ post }),
+    body: JSON.stringify({ title: title }),
   });
-  console.log(response);
 };
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", async function () {
+  const { posts } = await getPosts();
+  populatePosts(posts);
+
   document.getElementById("submitButton").addEventListener("click", addPost);
 });
