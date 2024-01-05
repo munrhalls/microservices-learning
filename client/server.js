05e732f165125c673b96a942e366062bd698264b
Munrhalls <antarcticdepths71@gmail.com>
2024-01-05 01:20:38 +0100

    enable server -> index.html <- javscript files

--- client/server.js
+++ client/server.js
@@ -1,19 +1,45 @@
-const http = require("http");
 const fs = require("fs");
 const path = require("path");
-http
-  .createServer((req, res) => {
-    const filePath = path.join(__dirname, "index.html");
-    const contentType = "text/html";
+const fastify = require("fastify");
 
-    fs.readFile(filePath, (err, content) => {
-      if (err) {
-        res.writeHead(500);
-        res.end("Error loading index.html");
+const getContentType = function (extname) {
+  switch (extname) {
+    case ".html":
+      return "text/html";
+    case ".js":
+      return "application/javascript";
+    case ".css":
+      return "text/css";
+    case ".json":
+      return "application/json";
+    default:
+      return "text/plain";
+  }
+};
+
+const server = fastify();
+
+server.get("*", (req, res) => {
+  const filePath = path.join(
+    __dirname,
+    req.url === "/" ? "index.html" : req.url
+  );
+  const extname = path.extname(filePath);
+  const contentType = getContentType(extname);
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === "ENOENT") {
+        res.code(404).send("File not found");
       } else {
-        res.writeHead(200, { "Content-Type": contentType });
-        res.end(content, "utf-8");
+        res.code(500).send("Server error");
       }
-    });
-  })
-  .listen(3000, () => console.log("Server is running on port 3000"));
+    } else {
+      res.header("Content-Type", contentType).send(content);
+    }
+  });
+});
+
+server.listen({ port: 3000 }, () => {
+  console.log("Frontend service listening on port 3000");
+});
--- posts/server.js
+++ posts/server.js
@@ -1,7 +1,22 @@
-const http = require("http");
+const fastify = require("fastify");
+const app = fastify();
+const cors = require("cors");
+const { randomBytes } = require("crypto");
 
-http
-  .createServer((req, res) => {
-    res.end("This is my first response");
-  })
-  .listen(4000, () => console.log("Server is running on port 4000"));
+const posts = [];
+posts.push();
+
+app.get("/posts", (req, res) => {
+  res.send({ posts: posts });
+});
+
+app.post("/posts", (req, res) => {
+  const post = req.body;
+  post.id = randomBytes(4).toString("hex");
+  posts.push(post);
+  res.send({ posts: posts });
+});
+
+app.listen({ port: 4000 }, () => {
+  console.log("Posts listening on port 4000");
+});
