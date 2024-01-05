9e94176c98c8a457df4d8c147ce28dc4b5f8f871
Munrhalls <antarcticdepths71@gmail.com>
2024-01-05 03:31:20 +0100

    SERVICE CLIENT REQUESTS SERVICE B TO UPDATE

--- client/index.js
+++ client/index.js
@@ -1,7 +1,17 @@
-const addPost = function (e) {
+const addPost = async function (e) {
   e.preventDefault();
-  console.log(e);
-  console.log("what???");
+  const form = document.getElementById("formNewPost");
+  const formData = new FormData(form);
+  const post = formData.get("post");
+
+  const response = await fetch("http://localhost:4000/posts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ post }),
+  });
+  console.log(response);
 };
 
 document.addEventListener("DOMContentLoaded", function () {
--- posts/server.js
+++ posts/server.js
@@ -1,22 +1,25 @@
 const fastify = require("fastify");
 const app = fastify();
-const cors = require("cors");
+const cors = require("@fastify/cors");
 const { randomBytes } = require("crypto");
 
 const posts = [];
-posts.push();
 
-app.get("/posts", (req, res) => {
-  res.send({ posts: posts });
-});
+(async function () {
+  await app.register(cors);
 
-app.post("/posts", (req, res) => {
-  const post = req.body;
-  post.id = randomBytes(4).toString("hex");
-  posts.push(post);
-  res.send({ posts: posts });
-});
+  app.get("/posts", (req, res) => {
+    res.send({ posts: posts });
+  });
 
-app.listen({ port: 4000 }, () => {
-  console.log("Posts listening on port 4000");
-});
+  app.post("/posts", (req, res) => {
+    const post = req.body;
+    post.id = randomBytes(4).toString("hex");
+    posts.push(post);
+    res.send({ posts: posts });
+  });
+
+  app.listen({ port: 4000 }, () => {
+    console.log("Posts listening on port 4000");
+  });
+})();
