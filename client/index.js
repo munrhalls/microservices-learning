528fe24d0dac4eb171e3ed51ff43c3e94c3e5768
Munrhalls <antarcticdepths71@gmail.com>
2024-01-05 03:04:29 +0100

    .

--- client/index.js
+++ client/index.js
@@ -1,3 +1,9 @@
 const addPost = function (e) {
+  e.preventDefault();
   console.log(e);
+  console.log("what???");
 };
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("submitButton").addEventListener("click", addPost);
+});
