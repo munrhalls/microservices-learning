1b465abf8e5a0e7ddfd2b87b277ce0b29c1ea4e8
Munrhalls <antarcticdepths71@gmail.com>
2024-01-06 17:50:49 +0100

    debug query services

--- query-service/server.js
+++ query-service/server.js
@@ -2,26 +2,37 @@ const fastify = require("fastify");
 const app = fastify();
 const cors = require("@fastify/cors");
 
-let posts = [];
+const getPosts = async () => {
+  await app.register(cors);
+  const data = await fetch("http://localhost:4000/posts");
+  const { posts } = await data.json();
+  return posts;
+};
 
 (async function () {
-  await app.register(cors);
+  const posts = await getPosts();
 
-  const data = await fetch("http://localhost:4000/posts");
-  posts = await data.json();
-  // SENDING EFFICIENT-REQUESTS FORMAT DATA
   app.get("/posts", async (req, res) => {
     res.send(posts);
   });
-  // RECEIVING EVENTS FROM EVENT BUS
-  app.post("/events", async (req, res) => {
-    console.log("QUERY SERVICE: RECEIVING EVENT FROM EVENT BUS");
-    console.log("QUERY SERVICE: RECEIVING EVENT FROM EVENT BUS");
-    console.log("QUERY SERVICE: RECEIVING EVENT FROM EVENT BUS");
-    console.log("QUERY SERVICE: RECEIVING EVENT FROM EVENT BUS");
 
+  app.post("/events", async (req, res) => {
     const { type, data } = req.body;
-    console.log("Received event:", type);
+    console.log("TYPE CHECK", req.body);
+    console.log(type === "PostCreated");
+    if (type === "PostCreated") {
+      console.log("?");
+      console.log("Data to be pushed: ", data);
+
+      try {
+        console.log("POSTS: ", posts);
+        debugger;
+        posts.push(data);
+      } catch (e) {
+        console.log("Error: ", e);
+      }
+      console.log("Updating posts, if check passes, posts: ", posts);
+    }
   });
 
   app.listen({ port: 4005 }, () => {
